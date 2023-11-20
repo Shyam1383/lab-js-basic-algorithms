a7b430af141b7b61894bfd945a3d500342965a8e
SHYAM KUMAR REDDY SANGATI <shyamkumarreddy515425@gmail.com>
2023-11-20 11:29:51 +0530

    NamesAndIterationsLoops

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
 // Iteration 1: Names and Input
-//
+let hacker1=prompt("enter Driver Name: ");
+console.log(`Driver's name is ${hacker1}`);
+
+let hacker2=prompt("Enter Navigator name: ");
+console.log("The navigator's name is "+hacker2);
+
 // Iteration 2: Conditionals
 
-
+let n=hacker1.length;
+let m=hacker2.length;
+if(n>m){
+  console.log("The driver has the longest name, it has "+n+ " characters");
+}
+else if(n<m){
+  console.log("It seems that the navigator has the longest name, it has " +m+ " characters");
+}
+else{
+  console.log("Wow, you both have equally long names, " +n+ " characters!");
+}
 // Iteration 3: Loops
-
-
-console.log("I'm ready!");
-
+for(let i=0;i<hacker1.length;i++){
+  console.log(hacker1[i].toUpperCase());
+}
+let rev="";
+for(let i=hacker2.length-1;i>=0;i--){
+  rev=rev+hacker2[i];
+}
+console.log(rev);
